784d2a21633f3513a4a61710f0b58f47368b2a31
Alejandro Acho <alejandroacho@hotmail.com>
2020-11-22 23:57:07 +0100

    Factories made

--- web/database/factories/ContactFactory.php
+++ web/database/factories/ContactFactory.php
@@ -7,22 +7,15 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ContactFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Contact::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'message' => $this->faker->paragraphs($nb = 3, $asText = false),
+            'birthday' => $this->faker->date($format = 'm-d', $max = 'now'),
         ];
     }
 }
