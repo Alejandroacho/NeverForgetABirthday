bdebb9d1eddc11e6f6d9c34c5d17dc965ccd4a5f
Alejandro Acho <alejandroacho@hotmail.com>
2020-11-24 23:58:30 +0100

    Coverage test added

--- web/tests/Feature/ContactControllerTest.php
+++ web/tests/Feature/ContactControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use \App\Models\User as User;
+use \App\Models\Contact as Contact;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -9,11 +11,205 @@ class ContactControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_if_contact_has_user_associated()
+    public function test_if_contact_has_name()
     {
-        $users = \App\Models\User::factory(10)->create();
-        $contact = \App\Models\Contact::factory()->create();
-        $user_id = $contact->user_id;
-        $this->assertNotNull($user_id);
+        $user = User::factory()->create();
+        $name = $user->name;
+        $this->assertNotNull($name);
     }
+
+    public function test_if_contact_has_lastname()
+    {
+        $user = User::factory()->create();
+        $lastname = $user->lastname;
+        $this->assertNotNull($lastname);
+    }
+
+    public function test_if_contact_has_email()
+    {
+        $user = User::factory()->create();
+        $email = $user->email;
+        $this->assertNotNull($email);
+    }
+
+    public function test_user_logged_can_access_home()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/home');
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function test_user_logged_can_access_create_contact_page()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/contact/create');
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function test_user_logged_can_add_a_new_contact()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->post('/contact', [
+            "name" => "Testing Contact",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+
+        $this->assertDatabaseHas('contacts',[
+            "name" => "Testing Contact",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/home');
+    }
+
+    public function test_user_logged_can_access_edit_contact_page()
+    {
+        $user = User::factory()->create();
+        $contact = new Contact;
+            $contact->name = "Testing Contact";
+            $contact->email = "testing@contact.com";
+            $contact->message = "Testing text";
+            $contact->birthday = "10/09/1995";
+            $contact->user_id = $user->id;
+            $contact->save();
+        $response = $this->actingAs($user)->get("/contact/".$contact->id."/edit");
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function test_if_user_logged_can_update_a_contact()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->post('/contact', [
+            "name" => "Testing Contact",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+
+        $response = $this->actingAs($user)->patch('/contact/1', [
+            "name" => "Testing Contact 2",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+
+        $this->assertDatabaseHas('contacts',[
+            "name" => "Testing Contact 2",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/home');
+    }
+
+    public function test_user_logged_can_see_its_contact()
+    {
+        $user = User::factory()->create();
+        $contact = new Contact;
+            $contact->name = "Testing Contact";
+            $contact->email = "testing@contact.com";
+            $contact->message = "Testing text";
+            $contact->birthday = "10/09/1995";
+            $contact->user_id = $user->id;
+            $contact->save();
+        $response = $this->actingAs($user)->get("/contact/".$contact->id);
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function test_user_logged_can_delete_a_contact()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user)->post('/contact', [
+            "name" => "Testing Contact",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+        $response = $this->actingAs($user)->delete("/contact/1");
+        $this->assertDatabaseMissing('contacts',[
+            "name" => "Testing Contact"
+        ]);
+        $this->assertEquals(302, $response->status());
+    }
+
+    public function test_id_user_cannot_see_contact_from_other_user()
+    {
+        $user = User::factory()->create();
+        $userTwo = User::factory()->create();
+        $this->actingAs($user)->post('/contact', [
+            "name" => "Testing Contact",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+        $response = $this->actingAs($userTwo)->get("/contact/1");
+        $response->assertViewIs('nopermission');
+    }
+
+    public function test_id_user_cannot_edit_contact_from_other_user()
+    {
+        $user = User::factory()->create();
+        $userTwo = User::factory()->create();
+        $this->actingAs($user)->post('/contact', [
+            "name" => "Testing Contact",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+        $response = $this->actingAs($userTwo)->get("/contact/1/edit");
+        $response->assertViewIs('nopermission');
+    }
+
+    public function test_if_other_user_cannot_update_a_contact_from_other_user()
+    {
+        $user = User::factory()->create();
+        $userTwo = User::factory()->create();
+        $this->actingAs($user)->post('/contact', [
+            "name" => "Testing Contact",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+
+        $response = $this->actingAs($userTwo)->patch('/contact/1', [
+            "name" => "Testing Contact 2",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+        $response->assertViewIs('nopermission');
+    }
+
+    public function test_id_user_cannot_delete_contact_from_other_user()
+    {
+        $user = User::factory()->create();
+        $userTwo = User::factory()->create();
+        $this->actingAs($user)->post('/contact', [
+            "name" => "Testing Contact",
+            "email" => "testing@contact.com",
+            "message" => "Testing text",
+            "birthday" => "10/09/1995",
+            "user_id" => $user->id,
+        ]);
+        $response = $this->actingAs($userTwo)->delete("/contact/1");
+        $response->assertViewIs('nopermission');
+    }
+
 }
--- web/tests/Feature/UserControllerTest.php
+++ web/tests/Feature/UserControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use \App\Models\User as User;
+use \App\Models\Contact as Contact;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -10,24 +11,60 @@ class UserControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_if_contact_has_name()
+    public function test_if_anyone_can_acces_index_page()
     {
-        $user = User::factory()->create();
-        $name = $user->name;
-        $this->assertNotNull($name);
+        $response = $this->get('/');
+        $response->assertStatus(200);
     }
 
-    public function test_if_contact_has_lastname()
+    public function test_if_anyone_can_acces_register_page()
     {
-        $user = User::factory()->create();
-        $lastname = $user->lastname;
-        $this->assertNotNull($lastname);
+        $response = $this->get('/register');
+        $response->assertStatus(200);
     }
 
-    public function test_if_contact_has_email()
+    public function test_if_anyone_can_acces_login_page()
     {
-        $user = User::factory()->create();
-        $email = $user->email;
-        $this->assertNotNull($email);
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
+
+    public function test_if_index_view_has_minimum_expected()
+    {
+        $response = $this->get('/');
+        $response->assertSee('Never Forget A Birthday Anymore!');
+        $response->assertSee('Login');
+        $response->assertSee('Register');
+        $response->assertSee('START NOW');
+        $response->assertSee('We will send him the letter you wrote at his birthday!');
+    }
+
+    public function test_if_login_view_has_minimum_expected()
+    {
+        $response = $this->get('/login');
+        $response->assertSee('Never Forget A Birthday');
+        $response->assertSee('Login');
+        $response->assertSee('E-Mail Address');
+        $response->assertSee('Password');
+    }
+
+    public function test_if_register_view_has_minimum_expected()
+    {
+        $response = $this->get('/register');
+        $response->assertSee('Never Forget A Birthday');
+        $response->assertSee('Register');
+        $response->assertSee('Name');
+        $response->assertSee('Lastname');
+        $response->assertSee('E-Mail Address');
+        $response->assertSee('Password');
+        $response->assertSee('Confirm Password');
+    }
+
+    public function test_if_contact_has_user_associated()
+    {
+        $users = User::factory(10)->create();
+        $contact = Contact::factory()->create();
+        $user_id = $contact->user_id;
+        $this->assertNotNull($user_id);
     }
 }
